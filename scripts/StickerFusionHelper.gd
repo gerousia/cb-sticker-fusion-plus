a4e60b6f01e74918eb867613aef6683bf4de500d
Gee <111046706+ginomari@users.noreply.github.com>
2025-04-21 15:56:37 +0800

    feat: update cost and value calculation formulas

--- scripts/StickerFusionHelper.gd
+++ scripts/StickerFusionHelper.gd
@@ -8,16 +8,16 @@ static func is_attribute_capped(attribute: StickerAttribute) -> bool:
 static func is_attribute_scaleable(attribute: StickerAttribute) -> bool:
 	return attribute.get("chance_max") or attribute.get("stat_value_max")
 
-static func merge_attributes(attributes: Array, attrib_a: StickerAttribute, attrib_b: StickerAttribute) -> void:
+static func merge_attributes(attributes: Array, attr_a: StickerAttribute, attr_b: StickerAttribute) -> void:
 	if attributes.size() <= 0:
 		return
-	if not attributes.has(attrib_a) and attributes.has(attrib_b):
+	if not attributes.has(attr_a) and attributes.has(attr_b):
 		return
-	if not attrib_a.get_template() == attrib_b.get_template():
+	if not attr_a.get_template() == attr_b.get_template():
 		return
 
-	var attrib_a_index = attributes.find(attrib_a)
-	var replacement = _copy_attribute(attributes[attrib_a_index])
+	var attr_a_index = attributes.find(attr_a)
+	var replacement = _copy_attribute(attributes[attr_a_index])
 
 	for field in ["chance", "stat_value"]:
 		var min_key = field + "_min"
@@ -25,14 +25,14 @@ static func merge_attributes(attributes: Array, attrib_a: StickerAttribute, attr
 		
 		if replacement.get(max_key):
 			replacement[field] = _get_scale_value(
-				min(attrib_a[field], attrib_b[field]),
-				max(attrib_a[field], attrib_b[field]),
+				min(attr_a[field], attr_b[field]),
+				max(attr_a[field], attr_b[field]),
 				replacement[min_key],
 				replacement[max_key]
 			)
 
-	attributes[attrib_a_index] = replacement
-	attributes.erase(attrib_b)	
+	attributes[attr_a_index] = replacement
+	attributes.erase(attr_b)	
 
 static func _copy_attribute(attribute: StickerAttribute) -> Resource:
 	var output = attribute.duplicate()	
@@ -52,30 +52,31 @@ static func _copy_attribute(attribute: StickerAttribute) -> Resource:
 static func get_upgrade_cost(duplicates: Dictionary) -> int:
 	var cost: int = 0
 
-	for attrib_a in duplicates:
-		var attrib_b = duplicates[attrib_a]
+	for attr_a in duplicates:
+		var attr_b = duplicates[attr_a]
 		
 		for field in ["chance", "stat_value"]:
 			var min_key = field + "_min"
 			var max_key = field + "_max"
 			
-			if attrib_a.get(max_key):
+			if attr_a.get(max_key):
 				var scaled_value = _get_scale_value(
-					min(attrib_a[field], attrib_b[field]),
-					max(attrib_a[field], attrib_b[field]),
-					attrib_a[min_key],
-					attrib_a[max_key]
+					min(attr_a[field], attr_b[field]),
+					max(attr_a[field], attr_b[field]),
+					attr_a[min_key],
+					attr_a[max_key]
 				)
-				var scaled_factor = _get_scale_factor(scaled_value, attrib_a.get(max_key))
-				cost += int(_get_interval_value(scaled_factor))
-
+				var scaled_factor = _get_scale_factor(scaled_value, attr_a.get("rarity"), attr_a.get(max_key))
+				cost += _get_interval_value(scaled_factor) * 0.5 # # # Too Expensive
+ 
 	return 100 * cost
 
-static func _get_scale_value(low: float, high: float, min_value: float, max_value: float, multiplier: float = 0.20) -> float:
-	return clamp(ceil(high + (low * multiplier)), min_value, max_value)
+static func _get_scale_value(low: float, high: float, min_value: float, max_value: float, rate: float = 0.5) -> float:
+	var formula = ceil(high + (low * (1 - high / max_value)) * rate) # # # Non-Linear Growth
+	return clamp(formula, min_value, max_value)
 	
-static func _get_scale_factor(value: float, max_value: float) -> float:
-	return ceil(100 * value / max_value)
+static func _get_scale_factor(value: float, rarity: int, max_value: float) -> float:
+	return 100 * (value / max_value) * rarity
 	
 static func _get_interval_value(value: float, interval: float = 20.0) -> float:
 	return ceil(value / interval) if value >= 0.0 else 0.0
